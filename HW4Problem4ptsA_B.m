957c5cc546762730cb45e12c5bcdbc7960b92057
Kyrithdagon <150574824+Kyrithdagon@users.noreply.github.com>
2023-11-16 22:35:19 -0800

    Add files via upload

---
+++ HW4Problem4ptsA_B.m
@@ -0,0 +1,14 @@
+%For problem 4 part a
+A = [2 -3 1; 5 10 6; 8 -7 5];
+
+T = rref(A) %finding reduced row echelon form
+
+S = rank(A) %rank of matrix
+
+
+%For problem 4 part b
+B = [-2 -4 4 5; 3 6 -6 -4; -2 -4 4 9];
+
+C = rref(B)
+
+D = rank(B)
